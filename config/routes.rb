4c9ccf642fc5bdf910c9c111a2bc388c024c675b
TreyDettmer <dettmertrey@gmail.com>
2023-08-25 12:06:39 -0700

    another attempt at fixing disabling signup

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,12 @@ Rails.application.routes.draw do
   resources :likes, only: [:create, :destroy]
   resources :applications, only: [:create, :destroy]
   get 'pages/media'
+
+  devise_scope :user do
+    get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
+    get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
+  end
+  
   devise_for :users, :skip => [:registrations]
     as :user do
       get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
