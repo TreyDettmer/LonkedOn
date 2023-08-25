5306e9b4f9c124f1c4d7614e3c191ae421ca8760
TreyDettmer <dettmertrey@gmail.com>
2023-08-25 12:37:29 -0700

    another attempt

--- config/routes.rb
+++ config/routes.rb
@@ -14,18 +14,11 @@ Rails.application.routes.draw do
   resources :likes, only: [:create, :destroy]
   resources :applications, only: [:create, :destroy]
   get 'pages/media'
-
-  devise_scope :user do
-    get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
-    get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
-  end
-  
   devise_for :users, :skip => [:registrations]
-    as :user do
-      get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
-      put 'users' => 'devise/registrations#update', :as => 'user_registration'
-    end
-  
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
 
   resources :users, only: [:show, :edit, :update] do 
     get 'build_a_new_work_experience', on: :member, defaults: { format: :turbo_stream }
